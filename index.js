0151f902a2b244f04f7d8d60f22d288078828765
Adi Oprician <adrianoprician@gmail.com>
2024-07-14 22:16:09 +0300

    add:initial structure,navbar,images

---
+++ index.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', displayAllProducts);
+const mainContainer = document.querySelector('.main');
+
+function getAllProducts(){
+    const url = "https://668d7a50099db4c579f31778.mockapi.io/products"
+    return fetch(url).then(response => response.json());
+} 
+function displayAllProducts(){
+    getAllProducts().then((products) => 
+    mainContainer.innerHTML = products.map(product =>
+        `
+        <div class="product-card">
+             <h3 class="card-title">${product.name}</h3>
+             <img src=${product.imageUrl} width="200px" />
+             <p class="card-price">${product.price}</p>
+        </div>
+        `
+    )).join('')
+    
+}
