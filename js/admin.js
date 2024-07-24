3ef5cbd77292e80fb26e99f34d5b0bbf91d3bd68
Adi Oprician <adrianoprician@gmail.com>
2024-07-24 20:56:37 +0300

    feat: delete, edit, postman collection

--- js/admin.js
+++ js/admin.js
@@ -11,6 +11,10 @@ document.addEventListener('DOMContentLoaded', displayAllProducts);
 function getAllProducts(){
     return fetch(url).then(response => response.json());
 } 
+function getProductById(id){
+    return fetch(`${url}/${id}`).then(response => response.json());
+} 
+
 function displayAllProducts() {
     getAllProducts().then(products => {
         productsTableBody.innerHTML = products.map(
@@ -18,24 +22,24 @@ function displayAllProducts() {
             <tr>
                 <td>${product.name}</td>
                 <td>${product.price}</td>
-                <td>
-              aici nu stiu  // <img src="${product.image}" alt="${product.name}">//
+                <td> <img src="${product.imageUrl}" alt="${product.name}" width="50px" />
                 </td>   
                 <td>${product.category}</td>
                 <td>
-                    <button>
+                    <button class="edit-${product.id}">
                         <i class="fa-solid fa-pen-to-square"></i>
                     </button>    
                 </td>
                 <td>
-                    <button>
+                    <button class="delete-${product.id}">
                        <i class="fa-solid fa-trash"></i>
                     </button>    
                 </td>
             </tr>
-            `)
+            `
+            )
             .join('');
-    })
+    });
 } 
 
 const form = document.getElementById('product-form');
@@ -45,9 +49,11 @@ const imageUrlInput = document.getElementById('image-url');
 const detailsInput = document.getElementById('details');
 const categoryInput = document.getElementById('category');
 const saveProductButton = document.getElementById('save-btn');
-
+let editMode = false;
+let currentEditableProductId;
 
 saveProductButton.addEventListener('click', saveProduct);
+
 
 console.log(nameInput, priceInput, imageUrlInput, detailsInput, categoryInput);
 
@@ -62,16 +68,52 @@ function saveProduct(event) {
         category: categoryInput.value,
     };
 
-    console.log(JSON.stringify(product),product);
-
-    fetch(url, {
-        method: 'POST',
+     fetch(editMode ? `${url}/${currentEditableProductId}` : url,{
+        method: editMode ? 'PUT' : 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify(product)
+        body: JSON.stringify(product),
     }).then(() => {
         form.reset();
         displayAllProducts();
-    })
+        editMode = false;
+    });
+}
+
+
+productsTableBody.addEventListener('click', handleActions);
+
+function handleActions(event) {
+    const className = event.target.parentElement.className;
+    if(className.includes('edit')){
+        const productId = className.split('-')[1];
+        editProduct(productId);
+    } else if(className.includes('delete')){
+        const productId = className.split('-')[1];
+        deleteProduct(productId);
+    }
+}
+    
+
+function editProduct(id) {
+    getProductById(id).then((product) => {
+        editMode = true;
+        nameInput.value = product.name;
+        priceInput.value = product.price;
+        imageUrlInput.value = product.imageUrl;
+        detailsInput.value = product.details;
+        categoryInput.value = product.category;
+
+        currentEditableProductId = product.id;
+    });
+}
+
+
+function deleteProduct(id) {
+    fetch(`${url}/${id}`, {
+        method: 'DELETE',
+    }).then(() => {
+        displayAllProducts();
+    });
 }
